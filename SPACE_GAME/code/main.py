c31a8270d140c6c2d310280636b6837d603b1e18
BrianC0des <bryansaavedrab272@gmail.com>
2025-04-02 20:16:00 +0800

    added new lines

--- SPACE_GAME/code/main.py
+++ SPACE_GAME/code/main.py
@@ -1,5 +1,22 @@
 import pygame # type: ignore
 
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+
 pygame.init()
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+running = True
+pygame.display.set_caption('Space Ship Game')
+
+while running: 
+    #event lool
+    for event in  pygame.event.get():
+       if event.type == pygame.QUIT:
+        running = False
+
+    #draw a game
+    display_surface.fill('blue')
+    pygame.display.update()
+
+
+
+pygame.quit()
