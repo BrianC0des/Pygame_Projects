6f27ff1130976735effe3386b1bedf583693675a
BrianC0des <bryansaavedrab272@gmail.com>
2025-04-03 20:31:51 +0800

    added images

--- SPACE_GAME/code/main.py
+++ SPACE_GAME/code/main.py
@@ -1,4 +1,8 @@
 import pygame # type: ignore
+from os.path import join
+import random
+
+
 
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 
@@ -7,11 +11,15 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Space Shooter')
 running = True
 
-
+#plain surface
 surf = pygame.Surface((100, 200))
 surf.fill('red')
 x = 100
 y = 150
+
+#importing image 
+
+player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
 
 while running: 
     #event lool
@@ -21,8 +29,8 @@ while running:
 
     #draw a game
     display_surface.fill('darkgray')
-    x += 2
-    display_surface.blit(surf, (x, y))
+    x += 0.1
+    display_surface.blit(player_surf, (x, y))
     pygame.display.update()
 
 
