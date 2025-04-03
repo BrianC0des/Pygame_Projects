79f0c9053216c828b9c10de9b834f1d7418be8f8
BrianC0des <bryansaavedrab272@gmail.com>
2025-04-03 19:41:07 +0800

    added codes

--- SPACE_GAME/code/main.py
+++ SPACE_GAME/code/main.py
@@ -4,8 +4,14 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 
 pygame.init()
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption('Space Shooter')
 running = True
-pygame.display.set_caption('Space Ship Game')
+
+
+surf = pygame.Surface((100, 200))
+surf.fill('red')
+x = 100
+y = 150
 
 while running: 
     #event lool
@@ -14,7 +20,9 @@ while running:
         running = False
 
     #draw a game
-    display_surface.fill('blue')
+    display_surface.fill('darkgray')
+    x += 2
+    display_surface.blit(surf, (x, y))
     pygame.display.update()
 
 
